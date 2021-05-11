9180cd68940f72651bee8191cd5d18090d346715
Haruhiko Takada <haruhiko.takada@gmail.com>
2021-05-12 02:06:08 +0900

    Slashコマンドの受け取りまで完了

--- app.py
+++ app.py
@@ -1,15 +1,15 @@
 from flask import Flask
+from flask import request
 app = Flask(__name__)
 
 
-@app.route("/")
+@app.route("/", methods=['GET', 'POST'])
 def hello():
-    return "Hello World!"
-
-
-@app.route("/cats")
-def cats():
-    return "Cats"
+    if request.method == 'POST':
+        # TODO リクエストの検証を行う
+        return "Hello %s" % request.form['text']
+    else:
+        return "Hello World!"
 
 
 if __name__ == "__main__":
